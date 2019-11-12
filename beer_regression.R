3f7887cd4f8fce063e1c802dd503a42cea4f6285
ScottChrist <scott.christ@gsa.gov>
2019-11-12 08:02:33 -0500

    updated R file

--- beer_regression.R
+++ beer_regression.R
@@ -1,4 +1,20 @@
 library(MASS)
 
 data <- read.csv(file="BeerDataScienceProject.csv")
+data[complete.cases(data), ]
 
+data.centered<-data.frame(scale(data,center=TRUE,scale=FALSE))
+
+model <- polr(as.factor(review_overall)~review_palette+review_appearance+review_taste+review_aroma,data=data,Hess=TRUE)
+
+#store table
+(ctable <- coef(summary(model)))
+
+## calculate and store p values
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+
+## combined table
+(ctable <- cbind(ctable, "p value" = p))
+
+#odds ratios
+exp(coef(model))
